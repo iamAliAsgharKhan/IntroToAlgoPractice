62e080988c2ec9bc2443f570f025e26a8719e319
Ali Asghar <iamaliasgharkhan@gmail.com>
2017-08-23 12:39:39 +0500

    Initial commit

---
+++ src/mergeSort/MergeSortImplementation.java
@@ -0,0 +1,23 @@
+package mergeSort;
+
+public class MergeSortImplementation {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+	
+	public static void merge(int array[]){
+		int p=0;    ///first index of the array
+		int q=array.length/2;  //mid of the array
+		int r=array.length;	   //final index of the array
+		
+		
+	}
+	
+	
+
+}
